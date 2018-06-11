8f001ca098f277d7fa0de1329f501c6aeed257d5
Kevin Iswara <kevin.iswara@tiket.com>
2018-06-11 20:53:15 +0700

    add profile fragment

---
+++ app/src/main/java/com/example/keviniswara/bookinglapang/profile/view/ProfileFragment.kt
@@ -0,0 +1,24 @@
+package com.example.keviniswara.bookinglapang.profile.view
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.keviniswara.bookinglapang.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
