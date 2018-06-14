8c682b121c426904de06309d7bbdc52d7edbdbbe
Kevin Iswara <kevin.iswara@tiket.com>
2018-06-14 14:30:17 +0700

    init database connection

---
+++ app/src/main/java/com/example/keviniswara/bookinglapang/utils/Database.kt
@@ -0,0 +1,73 @@
+package com.example.keviniswara.bookinglapang.utils
+
+import android.util.Log
+import com.example.keviniswara.bookinglapang.model.Order
+import com.example.keviniswara.bookinglapang.model.User
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+
+
+
+class Database {
+
+    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    val root: DatabaseReference = database.getReference("")
+
+    fun setUsers(user: User) {
+        val email = user.email.replace(".", "")
+        root.child("users").child(email).child("name").setValue(user.name)
+        root.child("users").child(email).child("phoneNumber").setValue(user.phoneNumber)
+        root.child("users").child(email).child("status").setValue(user.status)
+        root.child("users").child(email).child("email").setValue(email)
+        root.child("users").child(email).child("field").setValue(user.field)
+        root.child("users").child(email).child("orders").setValue(user.orders)
+    }
+
+    fun getUsers() {
+        val root: DatabaseReference = database.getReference("users")
+        root.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                Log.d("KEVIN", dataSnapshot.child("bookinglapang8@gmailcom").toString())
+
+                val user: User?
+
+                if (dataSnapshot.child("bookinglapang8@gmailcom").hasChild("field") &&
+                        dataSnapshot.child("bookinglapang8@gmailcom").hasChild("orders")) {
+                    user = dataSnapshot.child("bookinglapang8@gmailcom").getValue<User>(User::class.java)
+                } else  {
+                    val name: String = dataSnapshot.child("bookinglapang8@gmailcom").child("name")
+                            .getValue<String>(String::class.java) ?: ""
+                    val email: String = dataSnapshot.child("bookinglapang8@gmailcom").child("email")
+                            .getValue<String>(String::class.java) ?: ""
+                    val status: Int = dataSnapshot.child("bookinglapang8@gmailcom").child("status")
+                            .getValue<Int>(Int::class.java) ?: 0
+                    val phoneNumber: String = dataSnapshot.child("bookinglapang8@gmailcom").child("phoneNumber")
+                            .getValue<String>(String::class.java) ?: ""
+                    val field: String?
+                    val orders: List<Order>?
+
+                    if (dataSnapshot.child("bookinglapang8@gmailcom").hasChild("field")) {
+                        field = dataSnapshot.child("bookinglapang8@gmailcom").child("field")
+                                .getValue<String>(String::class.java)
+                    } else {
+                        field = null
+                    }
+
+                    orders = null
+
+                    user = User(name, email, phoneNumber, status, field, orders)
+                }
+                Log.d("KEVIN", user?.name)
+                Log.d("KEVIN", user?.phoneNumber)
+                Log.d("KEVIN", user?.status.toString())
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("KEVIN", "The read failed: " + databaseError.code)
+            }
+        })
+    }
+}
