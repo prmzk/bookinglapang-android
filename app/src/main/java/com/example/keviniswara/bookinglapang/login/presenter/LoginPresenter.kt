70b46533367bd62484797eec7f7d6d2d602d47f0
Kevin Iswara <kevin.iswara@tiket.com>
2018-06-15 11:04:02 +0700

    fix login in login module

--- app/src/main/java/com/example/keviniswara/bookinglapang/login/presenter/LoginPresenter.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/login/presenter/LoginPresenter.kt
@@ -1,10 +1,13 @@
 package com.example.keviniswara.bookinglapang.login.presenter
 
+import android.util.Log
 import com.example.keviniswara.bookinglapang.login.LoginContract
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
-class LoginPresenter : LoginContract.Presenter{
 
-
+class LoginPresenter() : LoginContract.Presenter {
 
     private var mView: LoginContract.View? = null
 
@@ -17,15 +20,28 @@ class LoginPresenter : LoginContract.Presenter{
     }
 
     override fun login() {
+
         var email = mView?.getEmail()
         var password = mView?.getPassword()
 
+        mView?.hideKeyboard()
+
         if (email == null || email == "") {
-
+            mView?.setErrorMessage("Email tidak boleh kosong.")
         } else if (password == null || password == "") {
-
+            mView?.setErrorMessage("Password tidak boleh kosong.")
         } else {
+            val mAuth = FirebaseAuth.getInstance()
 
+            val addOnCompleteListener: Any = mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener({ task ->
+                        if (task.isSuccessful) {
+                            mView?.startMainActivity()
+                        } else {
+                            Log.d("LOGIN ERROR", task.exception.toString())
+                            mView?.setErrorMessage("Login gagal.")
+                        }
+                    })
         }
     }
 }
--- app/src/main/java/com/example/keviniswara/bookinglapang/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/login/view/LoginActivity.kt
@@ -1,15 +1,18 @@
 package com.example.keviniswara.bookinglapang.login.view
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.keviniswara.bookinglapang.MainActivity
 import com.example.keviniswara.bookinglapang.R
 import com.example.keviniswara.bookinglapang.databinding.ActivitySignInBinding
 import com.example.keviniswara.bookinglapang.login.LoginContract
 import com.example.keviniswara.bookinglapang.login.presenter.LoginPresenter
-import kotlinx.android.synthetic.main.activity_sign_in.view.*
 
 class LoginActivity : AppCompatActivity(), LoginContract.View  {
 
@@ -38,9 +41,25 @@ class LoginActivity : AppCompatActivity(), LoginContract.View  {
             finish()
         })
     }
+
+    override fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun setErrorMessage(text: String) {
+        mBinding.errorMessage.text = text
+    }
+
     override fun initPresenter(): LoginContract.Presenter {
         val presenter: LoginPresenter = LoginPresenter()
         return presenter
+    }
+
+    override fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm!!.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
     }
 
     override fun getEmail(): String {
@@ -49,5 +68,9 @@ class LoginActivity : AppCompatActivity(), LoginContract.View  {
 
     override fun getPassword(): String {
         return mBinding.password.text.toString()
+    }
+
+    override fun getActivity(): Activity {
+        return this
     }
 }
