4c719c0127befa7f3521c4ea7560eb6ae7f08713
calmira <almiratjann@gmail.com>
2018-06-15 15:53:35 +0700

    add register error message from exception

--- app/src/main/java/com/example/keviniswara/bookinglapang/login/presenter/RegisterPresenter.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/login/presenter/RegisterPresenter.kt
@@ -8,6 +8,10 @@ import com.example.keviniswara.bookinglapang.login.RegisterContract
 import com.example.keviniswara.bookinglapang.login.view.RegisterActivity
 import com.example.keviniswara.bookinglapang.model.User
 import com.example.keviniswara.bookinglapang.utils.Database
+import com.google.firebase.auth.FirebaseAuthEmailException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 class RegisterPresenter : RegisterContract.Presenter {
 
@@ -40,19 +44,44 @@ class RegisterPresenter : RegisterContract.Presenter {
                             Database.setUsers(user)
                             mView!!.startMainActivity()
                         } else {
-                            Log.d("REGISTER", "Gagal")
+                            when {
+                                task.exception is FirebaseAuthWeakPasswordException -> {
+                                    Log.d("REGISTER", "Password should be at least 6 characters")
+                                    weakPasswordErrorMessage()
+                                }
+                                task.exception is FirebaseAuthInvalidCredentialsException -> {
+                                    Log.d("REGISTER", "The email address is badly formatted")
+                                    invalidEmailFormatErrorMessage()
+                                }
+                                task.exception is FirebaseAuthUserCollisionException -> {
+                                    Log.d("REGISTER", "The email address is already in use by another account.")
+                                    emailAlreadyInUseErrorMessage()
+                                }
+                            }
+                            Log.d("REGISTER", task.exception.toString())
                         }
-
                     })
         }
     }
 
     private fun confirmPasswordNotMatchErrorMessage() {
-        mView!!.getErrorMessage().text = "Password dan confirm password tidak cocok"
+        mView!!.getErrorMessage().text = "Password dan confirm password tidak cocok."
     }
 
     private fun nullErrorMessage(content: String) {
-        mView!!.getErrorMessage().text = content + " wajib diisi"
+        mView!!.getErrorMessage().text = content + " wajib diisi."
+    }
+
+    private fun weakPasswordErrorMessage() {
+        mView!!.getErrorMessage().text = "Password minimal terdiri dari 6 karakter."
+    }
+
+    private fun invalidEmailFormatErrorMessage() {
+        mView!!.getErrorMessage().text = "Email tidak valid."
+    }
+
+    private fun emailAlreadyInUseErrorMessage() {
+        mView!!.getErrorMessage().text = "Email sudah digunakan oleh akun lain."
     }
 
     override fun bind(view: RegisterContract.View) {
