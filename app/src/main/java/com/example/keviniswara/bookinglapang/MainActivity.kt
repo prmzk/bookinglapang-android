5e576365039b8a3abd0e36406154a39f898a0659
Kevin Iswara <kevin.iswara@tiket.com>
2018-06-15 16:13:58 +0700

    add button and background in home fragment

--- app/src/main/java/com/example/keviniswara/bookinglapang/MainActivity.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.keviniswara.bookinglapang
 
+import android.annotation.SuppressLint
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -7,12 +8,21 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.view.MenuItem
 import android.widget.FrameLayout
-import com.example.keviniswara.bookinglapang.R.attr.content
+import com.example.keviniswara.bookinglapang.R.drawable.*
 import com.example.keviniswara.bookinglapang.databinding.ActivityMainBinding
 import com.example.keviniswara.bookinglapang.home.view.HomeFragment
 import com.example.keviniswara.bookinglapang.order.view.OrderFragment
 import com.example.keviniswara.bookinglapang.profile.view.ProfileFragment
 import com.example.keviniswara.bookinglapang.status.view.StatusFragment
+import android.util.TypedValue
+import android.util.DisplayMetrics
+import android.view.ViewGroup
+import android.support.design.internal.BottomNavigationMenuView
+import android.view.View
+import android.support.design.internal.BottomNavigationItemView
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,21 +35,25 @@ class MainActivity : AppCompatActivity() {
                 R.id.bottom_nav_bar_1 -> {
                     val fragment = HomeFragment()
                     addFragment(fragment)
+                    item.setIcon(home_green)
                     return true
                 }
                 R.id.bottom_nav_bar_2 -> {
                     val fragment = OrderFragment()
                     addFragment(fragment)
+                    item.setIcon(order_green)
                     return true
                 }
                 R.id.bottom_nav_bar_3 -> {
                     var fragment = StatusFragment()
                     addFragment(fragment)
+                    item.setIcon(status_green)
                     return true
                 }
                 R.id.bottom_nav_bar_4 -> {
                     var fragment = ProfileFragment()
                     addFragment(fragment)
+                    item.setIcon(profile_green)
                     return true
                 }
             }
@@ -48,14 +62,31 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val mBinding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         content = mBinding.content
+
         val navigation = findViewById(R.id.bottom_nav_bar) as BottomNavigationView
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val menuView = navigation.getChildAt(0) as BottomNavigationMenuView
+        for (i in 0 until menuView.childCount) {
+            
+            // change icon size
+            val iconView = menuView.getChildAt(i).findViewById<View>(android.support.design.R.id.icon)
+            val layoutParams = iconView.getLayoutParams()
+            val displayMetrics = resources.displayMetrics
+            layoutParams.height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48f, displayMetrics).toInt()
+            layoutParams.width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48f, displayMetrics).toInt()
+            iconView.setLayoutParams(layoutParams)
 
+            // hide title
+            val item = menuView.getChildAt(i) as BottomNavigationItemView
+            item.setShiftingMode(false)
+            item.setChecked(item.itemData.isChecked)
+        }
 
         val fragment = HomeFragment()
         addFragment(fragment)
--- app/src/main/java/com/example/keviniswara/bookinglapang/home/view/HomeFragment.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/home/view/HomeFragment.kt
@@ -1,14 +1,20 @@
 package com.example.keviniswara.bookinglapang.home.view
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 
 import com.example.keviniswara.bookinglapang.R
+import com.example.keviniswara.bookinglapang.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
@@ -18,7 +24,53 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val mBinding : FragmentHomeBinding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_home, container, false)
+
+        val view: View = mBinding.root
+
+        var parentHeight = 0
+        var buttonHeight = 0
+        var parentWidth = 0
+        var buttonWidth = 0
+
+        view.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
+            override fun onGlobalLayout() {
+                parentHeight = view.height
+                buttonHeight = mBinding.buttonCariLapangan.height
+                parentWidth = view.width
+                buttonWidth = mBinding.buttonCariLapangan.width
+
+                val buttonCariLapanganY: Float = ((parentHeight - buttonHeight * 13 / 8)/ 2).toFloat()
+
+                val buttonCariLawanY: Float = (buttonCariLapanganY + buttonHeight * 3 / 4)
+
+                val buttonCariLapanganX: Float = ((parentWidth / 2 - buttonWidth * 5 / 6)).toFloat()
+
+                val buttonCariLawanX: Float = (buttonCariLapanganX + buttonWidth * 2 / 3)
+
+                mBinding.buttonCariLapangan.y = buttonCariLapanganY
+
+                mBinding.buttonCariLawan.y = buttonCariLawanY
+
+                mBinding.buttonCariLapangan.x = buttonCariLapanganX
+
+                mBinding.buttonCariLawan.x = buttonCariLawanX
+
+                if (parentHeight > 0 && buttonHeight > 0) {
+                    view.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            }
+        })
+
+        mBinding.buttonCariLapangan.setOnClickListener(View.OnClickListener {
+            mBinding.lalala.setText("cari lapangan coy")
+        })
+        mBinding.buttonCariLawan.setOnClickListener(View.OnClickListener {
+            mBinding.lalala.setText("cari lawan coy")
+        })
+        return view
     }
 }
