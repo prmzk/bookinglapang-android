f0059923a0b23ebd254a4cd75f721cbecd9b2eae
calmira <almiratjann@gmail.com>
2018-06-16 09:22:23 +0700

    add default constructor for field model

--- app/src/main/java/com/example/keviniswara/bookinglapang/model/Field.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/model/Field.kt
@@ -1,8 +1,12 @@
 package com.example.keviniswara.bookinglapang.model
 
-class Field(var field_id: String, var sports: List<Sport>) {
+class Field(var field_id: String = "",
+            var sports: List<Sport> = emptyList()) {
 
-    class Sport(var sport_name: String, var price_list: List<Price>)
+    class Sport(var sport_name: String = "",
+                var price_list: List<Price> = emptyList())
 
-    class Price(var day: String, var hour: String, var price: Int)
+    class Price(var day: String = "",
+                var hour: String = "",
+                var price: Int = 0)
 }
