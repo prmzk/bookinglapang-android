60e9b6807d7bf21edd3752dc5a9a013891dde004
Kevin Iswara <kevin.iswara@tiket.com>
2018-06-18 19:07:03 +0700

    Revert "add order list in recycler view"
    This reverts commit 6d5ed1883fbc1570f9935ae4ea6cd306cae71665.

--- app/src/main/java/com/example/keviniswara/bookinglapang/order/view/OrderFragment.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/order/view/OrderFragment.kt
@@ -1,78 +1,24 @@
 package com.example.keviniswara.bookinglapang.order.view
 
-import android.databinding.DataBindingUtil
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.keviniswara.bookinglapang.R
-import com.example.keviniswara.bookinglapang.databinding.FragmentOrderBinding
-import com.example.keviniswara.bookinglapang.model.Order
-import com.example.keviniswara.bookinglapang.order.OrderContact
-import com.example.keviniswara.bookinglapang.order.adapter.OrderAdapter
-import com.example.keviniswara.bookinglapang.order.presenter.OrderPresenter
-import android.support.v7.widget.DividerItemDecoration
 
+class OrderFragment : Fragment() {
 
-
-class OrderFragment : Fragment(), OrderContact.View{
-
-    private lateinit var mBinding: FragmentOrderBinding
-    private lateinit var mRecyclerView: RecyclerView
-    private lateinit var mPresenter: OrderPresenter
-    private lateinit var linearLayoutManager:LinearLayoutManager
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_order, container, false)
-
-        mPresenter = initPresenter()
-
-        mPresenter.bind(this)
-
-        mPresenter.retrieveOrderList()
-
-        linearLayoutManager = LinearLayoutManager(this.activity)
-
-        mRecyclerView = mBinding.rvOrder
-
-        val dividerItemDecoration = DividerItemDecoration(this.activity,
-                linearLayoutManager.orientation)
-
-        mRecyclerView.addItemDecoration(dividerItemDecoration)
-
-        val orders: MutableList<Order?> = mutableListOf()
-        for (i in 1..5) {
-            orders.add(Order())
-        }
-        mRecyclerView.adapter = OrderAdapter(orders)
-
-        return mBinding.root
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-    companion object {
-        fun newInstance(): OrderFragment {
-            val fragment = OrderFragment()
-            return fragment
-        }
-    }
-
-    override fun initPresenter(): OrderPresenter {
-        val presenter = OrderPresenter()
-        return presenter
-    }
-
-    override fun initListOfOrders(orders: MutableList<Order?>?) {
-        if (orders != null) {
-            mRecyclerView.layoutManager = linearLayoutManager
-            mRecyclerView.adapter = OrderAdapter(orders)
-        } else {
-            Log.d("KEVIN INIT", "masuk null")
-        }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_order, container, false)
     }
 }
