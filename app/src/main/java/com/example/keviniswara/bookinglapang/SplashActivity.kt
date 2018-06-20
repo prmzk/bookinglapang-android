2ce9767be8fc44b3ce495c699cba328219b18e7f
calmira <almiratjann@gmail.com>
2018-06-20 11:58:01 +0700

    add sign out

--- app/src/main/java/com/example/keviniswara/bookinglapang/SplashActivity.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/SplashActivity.kt
@@ -4,12 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.keviniswara.bookinglapang.login.view.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
+
+    private val mAuth =  FirebaseAuth.getInstance().currentUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
+        if (mAuth == null) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/keviniswara/bookinglapang/profile/view/ProfileFragment.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/profile/view/ProfileFragment.kt
@@ -1,16 +1,25 @@
 package com.example.keviniswara.bookinglapang.profile.view
 
 import android.content.Context
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.keviniswara.bookinglapang.MainActivity
 
 import com.example.keviniswara.bookinglapang.R
+import com.example.keviniswara.bookinglapang.databinding.FragmentProfileBinding
+import com.example.keviniswara.bookinglapang.login.view.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentProfileBinding
+    private val mAuth =  FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +27,15 @@ class ProfileFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile,
+                container, false)
+
+        mBinding.buttonLogout.setOnClickListener(View.OnClickListener {
+            mAuth.signOut()
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+        })
+
+        return mBinding.root
     }
 }
