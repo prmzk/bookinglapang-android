f991ac90c46732d3e444d5e000a37a30431ca04b
Kevin Iswara <kevin.iswara@tiket.com>
2018-06-27 10:23:26 +0700

    add status fragment in keeper directory

---
+++ app/src/main/java/com/example/keviniswara/bookinglapang/keeper/status/view/KeeperStatusDetailFragment.kt
@@ -0,0 +1,82 @@
+package com.example.keviniswara.bookinglapang.keeper.status.view
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.keviniswara.bookinglapang.R
+import com.example.keviniswara.bookinglapang.databinding.FragmentKeeperStatusDetailBinding
+import com.example.keviniswara.bookinglapang.keeper.status.KeeperStatusDetailContract
+import com.example.keviniswara.bookinglapang.keeper.status.presenter.KeeperStatusDetailPresenter
+
+class KeeperStatusDetailFragment : Fragment(), KeeperStatusDetailContract.View {
+
+    private lateinit var mPresenter: KeeperStatusDetailPresenter
+    private lateinit var mBinding: FragmentKeeperStatusDetailBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_keeper_status_detail, container, false)
+
+        mPresenter = initPresenter()
+
+        mPresenter.bind(this)
+
+        val sport = arguments!!.getString("sport")
+        val startHour = arguments!!.getString("startHour")
+        val endHour = arguments!!.getString("endHour")
+        val customerEmail = arguments!!.getString("customerEmail")
+        val status = arguments!!.getString("status")
+        val date = arguments!!.getString("date")
+        val fieldId = arguments!!.getString("fieldId")
+        val orderId = arguments!!.getString("orderId")
+
+        mPresenter.initOrderDetail(sport, startHour, endHour, customerEmail, status, date, fieldId)
+
+        mBinding.availableButton.setOnClickListener(View.OnClickListener {
+            mPresenter.setField(orderId, 0)
+        })
+
+        mBinding.notAvailableButton.setOnClickListener(View.OnClickListener {
+            mPresenter.setField(orderId, 1)
+        })
+        return mBinding.root
+    }
+
+    override fun setFieldId(fieldId: String) {
+        mBinding.fieldId.text = fieldId
+    }
+
+    override fun setSport(sport: String) {
+        mBinding.sport.text = sport
+    }
+
+    override fun setStartHour(startHour: String) {
+        mBinding.startHour.text = startHour
+    }
+
+    override fun setEndHour(endHour: String) {
+        mBinding.endHour.text = endHour
+    }
+
+    override fun setDate(date: String) {
+        mBinding.date.text = date
+    }
+
+    override fun initPresenter(): KeeperStatusDetailPresenter {
+        return KeeperStatusDetailPresenter()
+    }
+
+    override fun makeToast(text: String) {
+        Toast.makeText(activity, text, Toast.LENGTH_LONG).show()
+    }
+
+    override fun finish() {
+        val fm: FragmentManager? = fragmentManager
+        fm?.popBackStack()
+    }
+}
--- app/src/main/java/com/example/keviniswara/bookinglapang/keeper/status/view/KeeperStatusFragment.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/keeper/status/view/KeeperStatusFragment.kt
@@ -3,20 +3,88 @@ package com.example.keviniswara.bookinglapang.keeper.status.view
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.keviniswara.bookinglapang.R
 import com.example.keviniswara.bookinglapang.databinding.FragmentKeeperStatusBinding
+import com.example.keviniswara.bookinglapang.keeper.status.KeeperStatusContract
+import com.example.keviniswara.bookinglapang.keeper.status.adapter.KeeperStatusAdapter
+import com.example.keviniswara.bookinglapang.keeper.status.presenter.KeeperStatusPresenter
+import com.example.keviniswara.bookinglapang.model.Order
 
-class KeeperStatusFragment : Fragment() {
+class KeeperStatusFragment : Fragment(), KeeperStatusContract.View {
 
     private lateinit var mBinding: FragmentKeeperStatusBinding
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mPresenter: KeeperStatusPresenter
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private var mAdapter: KeeperStatusAdapter? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_keeper_status, container, false)
 
+        mPresenter = initPresenter()
+
+        mPresenter.bind(this)
+
+        linearLayoutManager = LinearLayoutManager(context)
+
+        mRecyclerView = mBinding.rvOrder
+
+        val dividerItemDecoration = DividerItemDecoration(this.activity,
+                linearLayoutManager.orientation)
+
+        mRecyclerView.addItemDecoration(dividerItemDecoration)
+
+
         return mBinding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mPresenter.retrieveOrderList()
+    }
+
+    override fun initListOfOrders(orders: MutableList<Order?>?) {
+        mRecyclerView.layoutManager = linearLayoutManager
+        mAdapter = KeeperStatusAdapter(orders, this)
+        mRecyclerView.adapter = mAdapter
+    }
+
+    override fun moveToDetail(orderDetail: Order) {
+        val arguments = Bundle()
+        val fragment = KeeperStatusDetailFragment()
+        arguments.putString("startHour", orderDetail.startHour)
+        arguments.putString("endHour", orderDetail.endHour)
+        arguments.putString("customerEmail", orderDetail.customerEmail)
+        arguments.putString("status", orderDetail.status.toString())
+        arguments.putString("date", orderDetail.date)
+        arguments.putString("sport", orderDetail.sport)
+        arguments.putString("fieldId", orderDetail.fieldId)
+        arguments.putString("orderId", orderDetail.orderId)
+        fragment.arguments = arguments
+        val ft = fragmentManager!!.beginTransaction()
+        ft.replace(R.id.content, fragment).addToBackStack(fragment
+                .javaClass.simpleName)
+        ft.commit()
+    }
+
+    override fun clearOrderList() {
+        mAdapter?.clearOrderList()
+    }
+
+    override fun makeToast(text: String) {
+        Toast.makeText(activity, text, Toast.LENGTH_LONG).show()
+    }
+
+    override fun initPresenter(): KeeperStatusPresenter {
+        return KeeperStatusPresenter()
     }
 }
