280c936bd76b298c2bc9f5b32dce7121f3b9781c
calmira <almiratjann@gmail.com>
2018-06-27 19:41:42 +0700

    add total payment calculation

--- app/src/main/java/com/example/keviniswara/bookinglapang/status/view/Payment3Fragment.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/status/view/Payment3Fragment.kt
@@ -6,8 +6,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.keviniswara.bookinglapang.R
 import com.example.keviniswara.bookinglapang.databinding.FragmentPayment3Binding
+import com.example.keviniswara.bookinglapang.home.view.HomeFragment
 import com.example.keviniswara.bookinglapang.status.Payment3Contract
 import com.example.keviniswara.bookinglapang.status.presenter.Payment3Presenter
 
@@ -24,10 +26,52 @@ class Payment3Fragment: Fragment(), Payment3Contract.View {
 
         mPresenter.bind(this)
 
+        val sport = arguments!!.getString("sport")
+        val startHour = arguments!!.getString("startHour")
+        val endHour = arguments!!.getString("endHour")
+        val customerEmail = arguments!!.getString("customerEmail")
+        val status = arguments!!.getString("status")
+        val date = arguments!!.getString("date")
+        val fieldId = arguments!!.getString("fieldId")
+        val deadline = arguments!!.getLong("deadline")
+        val orderId = arguments!!.getString("orderId")
+        val bankName = arguments!!.getString("bankName")
+
+        mPresenter.getBankDetail(bankName, orderId)
+
+        mPresenter.countTotalPayment(orderId, fieldId, sport, startHour, endHour, date)
+
+        mBinding.buttonPay.setOnClickListener({
+            startHomeFragment()
+            showToastMessage("Silakan tunggu konfirmasi dari administrator.")
+        })
+
         return mBinding.root
     }
 
     override fun initPresenter(): Payment3Contract.Presenter {
         return Payment3Presenter()
+    }
+
+    override fun showToastMessage(message: String) {
+        Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun startHomeFragment() {
+        val ft = fragmentManager!!.beginTransaction()
+        ft.replace(R.id.content, HomeFragment())
+        ft.commit()
+    }
+
+    override fun setName(name: String) {
+        mBinding.name.setText(name)
+    }
+
+    override fun setBillsNumber(billsNumber: String) {
+        mBinding.billsNumber.setText(billsNumber)
+    }
+
+    override fun setTotal(total: String) {
+        mBinding.total.setText(total)
     }
 }
