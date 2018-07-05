b9bbb6021d9fb8af6fca6c77096594ec16185016
Kevin Iswara <kevin.iswara@tiket.com>
2018-07-05 21:38:18 +0700

    add number picker dialog in add order

--- app/src/main/java/com/example/keviniswara/bookinglapang/user/home/SearchFieldContract.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/user/home/SearchFieldContract.kt
@@ -11,6 +11,7 @@ interface SearchFieldContract {
         fun getStartHour(): String
         fun getFinishHour(): String
         fun initDatePicker()
+        fun initNumberPicker(title: String, minValue: Int, maxValue: Int, type: Int)
         fun initListOfFieldDropdown(listOfField: List<String>)
         fun initListOfSportDropdown(listOfSport: List<String>)
         fun showToastMessage(message: String)
