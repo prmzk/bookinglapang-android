8af20674a074cc7c2f03f58918dd801ff72e0a88
jrandiny <jrandiny2@gmail.com>
2019-02-11 19:52:40 +0700

    Add help button

--- app/src/main/java/com/example/keviniswara/bookinglapang/user/home/view/HomeFragment.kt
+++ app/src/main/java/com/example/keviniswara/bookinglapang/user/home/view/HomeFragment.kt
@@ -11,6 +11,9 @@ import android.view.ViewGroup
 import android.view.ViewTreeObserver
 import com.example.keviniswara.bookinglapang.R
 import com.example.keviniswara.bookinglapang.databinding.FragmentHomeBinding
+import android.content.DialogInterface
+import android.support.v7.app.AlertDialog
+
 
 class HomeFragment : Fragment() {
 
@@ -22,7 +25,7 @@ class HomeFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
 
         val mBinding: FragmentHomeBinding = DataBindingUtil.inflate(inflater,
-                R.layout.fragment_home, container, false)
+                com.example.keviniswara.bookinglapang.R.layout.fragment_home, container, false)
 
         val view: View = mBinding.root
 
@@ -41,19 +44,19 @@ class HomeFragment : Fragment() {
 
                 val buttonCariLapanganY: Float = ((parentHeight - buttonHeight * 13 / 8) / 2).toFloat()
 
-                val buttonCariLawanY: Float = (buttonCariLapanganY + buttonHeight * 3 / 4)
+                val buttonHomeY: Float = (buttonCariLapanganY + buttonHeight * 3 / 4)
 
                 val buttonCariLapanganX: Float = ((parentWidth / 2 - buttonWidth * 5 / 6)).toFloat()
 
-                val buttonCariLawanX: Float = (buttonCariLapanganX + buttonWidth * 2 / 3)
+                val buttonHomeX: Float = (buttonCariLapanganX + buttonWidth * 2 / 3)
 
                 mBinding.buttonCariLapangan.y = buttonCariLapanganY
 
-                mBinding.buttonCariLawan.y = buttonCariLawanY
+                mBinding.buttonHelp.y = buttonHomeY
 
                 mBinding.buttonCariLapangan.x = buttonCariLapanganX
 
-                mBinding.buttonCariLawan.x = buttonCariLawanX
+                mBinding.buttonHelp.x = buttonHomeX
 
                 if (parentHeight > 0 && buttonHeight > 0) {
                     view.viewTreeObserver.removeOnGlobalLayoutListener(this)
@@ -63,16 +66,26 @@ class HomeFragment : Fragment() {
 
         mBinding.buttonCariLapangan.setOnClickListener({
             val ft = fragmentManager!!.beginTransaction()
-            ft.replace(R.id.content, SearchFieldFragment()).addToBackStack(SearchFieldFragment()
+            ft.replace(com.example.keviniswara.bookinglapang.R.id.content, SearchFieldFragment()).addToBackStack(SearchFieldFragment()
                     .javaClass.simpleName)
             ft.commit()
         })
-        mBinding.buttonCariLawan.setOnClickListener({
-            val ft = fragmentManager!!.beginTransaction()
-            ft.replace(R.id.content, FindEnemyMainFragment()).addToBackStack(FindEnemyMainFragment()
-                    .javaClass.simpleName)
-            ft.commit()
-        })
+        mBinding.buttonHelp.setOnClickListener {
+            AlertDialog.Builder(this.context!!)
+                    .setTitle("Cara Pemesanan")
+                    .setMessage("1. Isi data lapangan yang ingin di cari ( Nama lapangan, Jenis Olahraga, Tanggal main, Jam main dan jam Selesai)\n" +
+                            "\n" +
+                            "2. Tunggu notifikasi pemesanan pada halaman Status Pemesanan. \n" +
+                            "- Jika status \" Belum Verifikasi \", penjaga lapangan belum menjawab pesanan\n" +
+                            "- Jika status \" lapangan tersedia \", lapangan yg dipesan tersedia dan user diharapkan segera melakukan pembayaran\n" +
+                            "-Jika status \" tidak tersedia \", lapangan yg dipesan tidak tersedia, silahkan mengajukan pesanan lain\n" +
+                            "\n" +
+                            "3. Jika lapangan tersedia pilih menu bayar. Pilih ingin melakukan pembayaran transfer ke bank apa, dan lakukan transfer ke no rekening bank yang tertera sesuai dengan biaya serta KODE UNIK-nya (3 angka paling belakang)\n" +
+                            "\n" +
+                            "4. Tunggu konfirmasi finalisasi. apabila sudah sudah dikonfirmasi untuk pemesanan dan pembayaran, KorbanBooking hanya tinggal datang ke lapangan pada waktu dan tempat yang telah dipesan. Selamat bermain :)")
+                    .setPositiveButton("Ok", DialogInterface.OnClickListener { dialog, which -> dialog.dismiss() })
+                    .show()
+        }
         return view
     }
 }
